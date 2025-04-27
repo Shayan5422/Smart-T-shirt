64a263c223f650493d415ce79521df8477db5507
Shayan Hashemi <83245719+Shayan5422@users.noreply.github.com>
2025-04-27 13:43:46 +0200

    Initial Commit

---
+++ Smart T-shirt/Smart_T_shirtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_T_shirtApp.swift
+//  Smart T-shirt
+//
+//  Created by Shayan Hashemi on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_T_shirtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smart T-shirtTests/Smart_T_shirtTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_T_shirtTests.swift
+//  Smart T-shirtTests
+//
+//  Created by Shayan Hashemi on 4/27/25.
+//
+
+import Testing
+@testable import Smart_T_shirt
+
+struct Smart_T_shirtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
